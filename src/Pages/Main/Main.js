e3c8f4edec958ebb1692a95a4c33f856042b225f
yeonjeongCho <jolove.dev@gmail.com>
2020-10-19 18:01:59 +0900

    Add: first commit. 초기세팅 완료

---
+++ src/Pages/Main/Main.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+class Main extends React.Component {
+  render() {
+    return <div>안녕하세요. :)</div>;
+  }
+}
+export default Main;
---
+++ src/Routes.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Main from "./Pages/Main/Main";
+// import Login from "./Pages/Login/Login";
+// import SignUp from "./Pages/SignUp";
+// import Feeds from "./Pages/Feeds";
+
+class Routes extends React.Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Main} />
+          {/* <Route exact path="/Login" component={Login} /> */}
+          {/* <Route exact path="/SignUp" component={SignUp} /> */}
+          {/* <Route exact path="/Feeds" component={Feeds} /> */}
+        </Switch>
+      </Router>
+    );
+  }
+}
+export default Routes;
