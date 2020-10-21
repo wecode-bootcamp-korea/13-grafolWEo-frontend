2b08f7d7c2b5b5ab4cf0f12be2ea83467ce91197
yeonjeongCho <jolove.dev@gmail.com>
2020-10-21 11:02:06 +0900

    Add:implement LoginSection

---
+++ src/Pages/Login/Login.js
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./Login.scss";
+
+class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      mostViewdImg:
+        "https://usercontents-c.styleshare.io/images/21484842/700x432",
+    };
+  }
+
+  render() {
+    return (
+      <div className="Login">
+        <section>
+          <div className="loginContainer">
+            <header>
+              <img className="logo" src="/Images/GrafolWeo.png" alt="logo" />
+              <span className="loginText">로그인</span>
+            </header>
+
+            <form>
+              <div className="idBox">
+                <label>이메일</label>
+                <input type="text" placeholder="example@naver.com" />
+              </div>
+              <div className="pwBox">
+                <label>비밀번호</label>
+                <input type="password" placeholder="password" />
+              </div>
+              <div className="optionBox">
+                <span>비밀번호를 잊으셨나요?</span>
+                <Link to="/SignUp">
+                  <span>회원가입하기</span>
+                </Link>
+              </div>
+              <button classNam="btnStyle" type="button">
+                로그인
+              </button>
+            </form>
+            <footer>
+              최초 로그인 시<br />
+              <span>이용약권, 개인정보 수집 및 이용, 개인정보 제공</span>에
+              <br /> 동의하는 것으로 간주합니다.
+            </footer>
+          </div>
+        </section>
+        <aside
+          style={{ backgroundImage: `url(${this.state.mostViewdImg})` }}
+        ></aside>
+      </div>
+    );
+  }
+}
+
+export default Login;
--- src/Routes.js
+++ src/Routes.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Main from "./Pages/Main/Main";
-// import Login from "./Pages/Login/Login";
-// import SignUp from "./Pages/SignUp";
+// import Main from "./Pages/Main/Main";
+import Login from "./Pages/Login/Login";
+import SignUp from "./Pages/SignUp/SignUp";
 // import Feeds from "./Pages/Feeds";
 
 class Routes extends React.Component {
@@ -10,9 +10,9 @@ class Routes extends React.Component {
     return (
       <Router>
         <Switch>
-          <Route exact path="/" component={Main} />
-          {/* <Route exact path="/Login" component={Login} /> */}
-          {/* <Route exact path="/SignUp" component={SignUp} /> */}
+          {/* <Route exact path="/" component={Main} /> */}
+          <Route exact path="/Login" component={Login} />
+          <Route exact path="/SignUp" component={SignUp} />
           {/* <Route exact path="/Feeds" component={Feeds} /> */}
         </Switch>
       </Router>
