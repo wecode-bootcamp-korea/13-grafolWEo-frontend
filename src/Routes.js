f8130ac3e39b083efae4ec8e120d00ddbe459e92
김보경 <akbk1004@gmail.com>
2020-10-23 15:08:04 +0900

    Add:Detail 페이지 레이아웃 구현

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Main from "./Pages/Main/Main";
+// import Main from "./Pages/Main/Main";
+import DetailPages from "./Components/DetailPages/DetailPages";
 // import Login from "./Pages/Login/Login";
 // import SignUp from "./Pages/SignUp";
 // import Feeds from "./Pages/Feeds";
@@ -10,7 +11,8 @@ class Routes extends React.Component {
     return (
       <Router>
         <Switch>
-          <Route exact path="/" component={Main} />
+          {/* <Route exact path="/" component={Main} /> */}
+          <Route exact path="/" component={DetailPages}/>
           {/* <Route exact path="/Login" component={Login} /> */}
           {/* <Route exact path="/SignUp" component={SignUp} /> */}
           {/* <Route exact path="/Feeds" component={Feeds} /> */}
