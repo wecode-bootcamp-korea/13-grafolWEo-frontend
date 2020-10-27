ea256b22ae34d095e1860287605b2b1bd1adbf94
yeonjeongCho <jolove.dev@gmail.com>
2020-10-27 11:42:02 +0900

    Modify:reflected reviews

--- src/config.js
+++ src/config.js
@@ -11,3 +11,5 @@ export const LISTBANNERBGSRC =
   "https://images.unsplash.com/photo-1536924940846-227afb31e2a5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1047&q=80";
 
 export const API = "http://localhost:3000";
+
+export const SH_URL = "http://10.58.0.139:8000";
