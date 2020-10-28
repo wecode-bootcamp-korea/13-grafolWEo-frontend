f7a0fb2b4a77c9481c03a3f15dadd4631ee31e7a
yeonjeongCho <jolove.dev@gmail.com>
2020-10-28 20:19:14 +0900

    Modify:로그인/로그아웃 감지 기능 추가

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -3,14 +3,6 @@ import { Link } from "react-router-dom";
 import { withRouter } from "react-router-dom";
 import "./Navbar.scss";
 
-// let controlActivation = [
-//   { id: 1, path: "/Login", content: "피드" },
-//   { id: 2, path: "/", content: "작품" },
-//   { id: 3, path: "/", content: "아트상품" },
-//   { id: 4, path: "/Wallpaper", content: "배경화면" },
-//   { id: 5, path: "", content: "스토리" },
-// ];
-
 let controlActivation = [
   { id: 2, path: "/", content: "작품" },
   { id: 3, path: "/", content: "아트상품" },
@@ -39,15 +31,20 @@ class Navbar extends Component {
     this.setState({ isHover: state });
   };
 
+  logout = () => {
+    console.log(this.state.isLogin);
+    localStorage.removeItem("Authorization");
+    this.setState({ isLogin: false });
+    controlActivation.shift();
+  };
+
   componentDidMount() {
     if (localStorage.getItem("Authorization")) {
       controlActivation.unshift({ id: 1, path: "/Login", content: "피드" });
       this.setState({ isLogin: true });
+    } else {
+      controlActivation.shift();
     }
-  }
-
-  logout() {
-    this.setState({ isLogin: false });
   }
 
   render() {
@@ -76,24 +73,6 @@ class Navbar extends Component {
           <li>
             <div className="more">
               <img src="/Images/menu.png" alt="more_icon" />
-              <div className="hideBox">
-                <div className="triangle">
-                  <ul className="moreSubMenu">
-                    <li>서비스 소개</li>
-                    <li>후원</li>
-                    <li>네이버 OGQ 마켓</li>
-                    <li>공식블로그</li>
-                    <li>제휴문의</li>
-                    <li>공지사항</li>
-                    <li>도움말</li>
-                    <li>한국어</li>
-                    <ul className="languages">
-                      <li>한국어</li>
-                      <li>English(US)</li>
-                    </ul>
-                  </ul>
-                </div>
-              </div>
             </div>
           </li>
         </ul>
@@ -107,15 +86,15 @@ class Navbar extends Component {
               <Link to="/Login">로그인</Link>
             </button>
           )}
-          <div className="upload">
+          <div className={`upload ${!isLogin ? "display-none" : ""}`}>
             <img src="/Images/upload.png" alt="upload_icon" />
             업로드
           </div>
-          <div>
+          <div className={`img ${!isLogin ? "display-none" : ""}`}>
             <img className="bellIcon" src="/Images/bell.png" alt="bell_icon" />
           </div>
           <div
-            className="user"
+            className={`user ${!isLogin ? "display-none" : ""}`}
             onMouseEnter={() => this.handleHover(true)}
             onMouseLeave={() => this.handleHover(false)}
           >
